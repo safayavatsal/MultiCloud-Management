4d2f15ff7576aeced686e1ab237d64e889f4d817
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-07 14:27:35 +0530

    Update main-variables.tf

--- Azure/main-variables.tf
+++ Azure/main-variables.tf
@@ -1,1 +1,54 @@
+variable "name" {
+  default = "xcloud-project"
+}
+
+variable "resource_group" {}
+
+variable "region" {
+  default = "West Europe"
+}
+
+variable "zones" {
+  default = ["1", "2"]
+}
+
+variable "env" {
+  default = "dev"
+}
+
+variable "network_name" {
+  default = "xcloud-network"
+}
+
+variable "image" {
+  default = "Canonical:UbuntuServer:18.04-LTS:latest"
+}
+
+variable "appserver_count" {
+  default = 1
+}
+
+variable "webserver_count" {
+  default = 2
+}
+
+variable "app_image" {
+  default = "OpenLogic:CentOS:7.9:latest"
+}
+
+variable "instance_type" {
+  default = "Standard_B1s"
+}
+
+variable "no_of_db_instances" {
+  default = 1
+}
+
+variable "enable_autoscaling" {
+  default = true
+}
+
+variable "db_user" {}
+
+variable "db_password" {}
 
