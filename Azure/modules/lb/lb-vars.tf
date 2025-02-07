e83de189bcbaf883660ffd6131546c5b57cb11a6
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-07 14:33:14 +0530

    Update lb-vars.tf

--- Azure/modules/lb/lb-vars.tf
+++ Azure/modules/lb/lb-vars.tf
@@ -1,1 +1,7 @@
+variable "name" {}
+variable "resource_group" {}  # Azure requires a resource group
+variable "region" {}
+variable "webserver_count" {}
+variable "vmss_id" {}  # Azure uses VM Scale Sets instead of instance templates
+variable "zones" { type = list }
 
