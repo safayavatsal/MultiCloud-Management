271d307370ea017641014459a7521bee2d827216
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-07 14:30:51 +0530

    Update instance-template.tf

--- Azure/modules/instance-template/instance-template.tf
+++ Azure/modules/instance-template/instance-template.tf
@@ -1,1 +1,48 @@
+resource "azurerm_linux_virtual_machine_scale_set" "ws_instance_template" {
+  count = var.enable_autoscaling ? 1 : 0
+
+  name                = "${var.name}-vmss"
+  resource_group_name = var.resource_group
+  location            = var.region
+  sku                 = var.instance_type
+  instances           = var.webserver_count
+  admin_username      = "adminuser"
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("${var.ssh_key}")
+  }
+
+  network_interface {
+    name    = "${var.name}-nic"
+    primary = true
+
+    ip_configuration {
+      name      = "internal"
+      subnet_id = var.subnet_id
+
+      public_ip_address {
+        allocation = "Dynamic"
+      }
+    }
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching             = "ReadWrite"
+  }
+
+  custom_data = base64encode(file("${path.module}/scripts/startup.sh"))
+
+  tags = {
+    environment = var.env
+  }
+}
 
