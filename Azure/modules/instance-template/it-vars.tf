47b84a7446ce836a2a7128c0d3020591eef563b7
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-07 14:32:26 +0530

    Update it-vars.tf

--- Azure/modules/instance-template/it-vars.tf
+++ Azure/modules/instance-template/it-vars.tf
@@ -1,1 +1,12 @@
+variable "name" {}
+variable "resource_group" {}  # Azure requires a resource group
+variable "vnet_name" {}  # Azure Virtual Network
+variable "subnet_id" {}  # Azure uses Subnet ID instead of `self_link`
+variable "instance_type" {}
+variable "enable_autoscaling" {}
+variable "env" {}
+variable "region" {}
+variable "image" {}
+variable "admin_username" { default = "azureuser" }  # Default admin user for VM
+variable "admin_password" {}  # Required if using password authentication
 
