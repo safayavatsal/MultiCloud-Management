e9a432eecdb5cce7c5d2c4bb2227a95743dca7cb
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-07 22:54:55 +0530

    Update ms-outputs.tf

--- Azure/modules/microservice-instance/ms-outputs.tf
+++ Azure/modules/microservice-instance/ms-outputs.tf
@@ -1,1 +1,8 @@
+#output "app_vm_private_ips" {
+#  value = { for key, vm in azurerm_linux_virtual_machine.apps : key => vm.private_ip_address }
+#}
+#
+#output "app_vm_public_ips" {
+#  value = { for key, nic in azurerm_network_interface.app_nic : key => nic.ip_configuration[0].public_ip_address }
+#}
 
