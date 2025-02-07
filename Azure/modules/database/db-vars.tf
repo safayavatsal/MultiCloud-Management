46410324a56754e5f025ba0f6b465e33c9c31136
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-07 14:29:42 +0530

    Update db-vars.tf

--- Azure/modules/database/db-vars.tf
+++ Azure/modules/database/db-vars.tf
@@ -1,1 +1,22 @@
+variable "resource_group" {
+  description = "The name of the resource group where resources will be created"
+  type        = string
+}
+
+variable "region" {
+  description = "Azure region where the resources will be deployed"
+  type        = string
+  default     = "East US"
+}
+
+variable "db_user" {
+  description = "The administrator username for the PostgreSQL database"
+  type        = string
+}
+
+variable "db_password" {
+  description = "The administrator password for the PostgreSQL database"
+  type        = string
+  sensitive   = true
+}
 
