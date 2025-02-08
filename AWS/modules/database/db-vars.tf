78969dfed1afb4e014c8235ea267ebc623b7c176
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-08 18:47:23 +0530

    Update db-vars.tf

--- AWS/modules/database/db-vars.tf
+++ AWS/modules/database/db-vars.tf
@@ -1,6 +1,30 @@
-# variable nat_ip{}
+variable "no_of_db_instances" {}
 
-variable no_of_db_instances{ }
-variable db_name {}
-variable db_user {}
-variable db_password {}
+variable "db_name" {}
+
+variable "db_user" {}
+
+variable "db_password" {
+  description = "The administrator password for the PostgreSQL database"
+  type        = string
+  sensitive   = true
+}
+
+# Disaster Recovery & Replication
+variable "gcp_replica_enabled" {
+  description = "Enable GCP read replica for cross-cloud disaster recovery"
+  type        = bool
+  default     = false
+}
+
+variable "azure_replica_enabled" {
+  description = "Enable Azure read replica for cross-cloud disaster recovery"
+  type        = bool
+  default     = false
+}
+
+variable "backup_retention_period" {
+  description = "Number of days to retain backups"
+  type        = number
+  default     = 7
+}
