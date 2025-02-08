78528eabedf3cd76e41101c920c113aaf6bef498
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-08 20:04:20 +0530

    Update ms-vars.tf

--- AWS/modules/microservice-instance/ms-vars.tf
+++ AWS/modules/microservice-instance/ms-vars.tf
@@ -1,5 +1,19 @@
-variable appserver_count {}
-variable instance_type {}
-variable image {}
-variable zones {}
-# variable primary_network_interface-id{}
+variable "appserver_count" {
+  type        = number
+  description = "Number of application server instances"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance type for the application servers"
+}
+
+variable "image" {
+  type        = string
+  description = "Amazon Machine Image (AMI) ID for the application servers"
+}
+
+variable "zones" {
+  type        = list(string)
+  description = "List of availability zones for deployment"
+}
