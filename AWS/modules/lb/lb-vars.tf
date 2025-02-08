6864d8d3c520166a3eb1439102c68095441be9af
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-08 18:58:35 +0530

    Update lb-vars.tf

--- AWS/modules/lb/lb-vars.tf
+++ AWS/modules/lb/lb-vars.tf
@@ -1,5 +1,24 @@
-variable image {}
-variable instance_type {}
-variable region {}
-variable zones { type = list}
-variable vpc-id{}
+variable "image" {}
+
+variable "instance_type" {}
+
+variable "region" {}
+
+variable "zones" { 
+  type = list
+}
+
+variable "vpc-id" {}
+
+# Multi-cloud Load Balancing & Failover
+variable "enable_cross_cloud_lb" {
+  description = "Enable cross-cloud load balancing with GCP and Azure"
+  type        = bool
+  default     = false
+}
+
+variable "enable_failover" {
+  description = "Enable failover routing in case AWS region goes down"
+  type        = bool
+  default     = true
+}
