35c546409832931646631b95885f54e3118f238f
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-08 19:00:00 +0530

    Update lb-vars.tf

--- GCP/modules/lb/lb-vars.tf
+++ GCP/modules/lb/lb-vars.tf
@@ -1,6 +1,26 @@
-variable name {}
-variable project {}
-variable region {}
-variable webserver_count { }
-variable ws-instance_template {}
-variable zones { type = list}
+variable "name" {}
+
+variable "project" {}
+
+variable "region" {}
+
+variable "webserver_count" {}
+
+variable "ws-instance_template" {}
+
+variable "zones" { 
+  type = list
+}
+
+# Multi-cloud Load Balancing & Failover
+variable "enable_cross_cloud_lb" {
+  description = "Enable cross-cloud load balancing with AWS and Azure"
+  type        = bool
+  default     = false
+}
+
+variable "enable_failover" {
+  description = "Enable failover routing in case GCP region goes down"
+  type        = bool
+  default     = true
+}
