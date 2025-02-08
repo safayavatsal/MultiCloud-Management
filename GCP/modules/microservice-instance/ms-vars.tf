a10585dc181f01497d54fb25e11724bb6fd6a550
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-08 20:06:41 +0530

    Update ms-vars.tf

--- GCP/modules/microservice-instance/ms-vars.tf
+++ GCP/modules/microservice-instance/ms-vars.tf
@@ -1,6 +1,29 @@
-variable appserver_count {}
-variable vpc-name{}
-variable subnet-self_link{}
-variable zones{}
-variable image{}
-variable instance_type{}
+variable "appserver_count" {
+  type        = number
+  description = "Number of application server instances"
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "VPC network name"
+}
+
+variable "subnet_self_link" {
+  type        = string
+  description = "Self-link of the subnet for app servers"
+}
+
+variable "zones" {
+  type        = list(string)
+  description = "List of availability zones for deployment"
+}
+
+variable "image" {
+  type        = string
+  description = "OS image reference for the VM instances"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance type for the application servers"
+}
