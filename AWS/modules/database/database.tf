ec06e6755ce23c282bd3951c60bad5d9a9922cbb
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-08 18:36:01 +0530

    Update database.tf

--- AWS/modules/database/database.tf
+++ AWS/modules/database/database.tf
@@ -1,16 +1,25 @@
 resource "random_id" "db_name_suffix" {
   byte_length = 4
 }
+
 resource "aws_db_instance" "xcloud_postgres_db" {
-  # count  = var.no_of_db_instances
-  name             = "postgres_instance_${random_id.db_name_suffix.hex}"
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "12.4"
-  instance_class       = "db.t2.micro"
-  username             = var.db_user
-  password             = var.db_password
+  name                   = "postgres_instance_${random_id.db_name_suffix.hex}"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "postgres"
+  engine_version         = "12.4"
+  instance_class         = "db.t2.micro"
+  username               = var.db_user
+  password               = var.db_password
   final_snapshot_identifier = "xcloud-postgres-db-backup"
-  skip_final_snapshot       = true
+  skip_final_snapshot    = false
+
+  backup_retention_period = 7
+  storage_encrypted       = true
+
+  # Enable Multi-AZ for DR
+  multi_az = true
+
+  # Enable Cross-Region Read Replica
+  replicate_source_db = var.gcp_replica_source
 }
