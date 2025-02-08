a842fa75363569d6bbbab35c8f7c25bc86d7430e
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-08 18:29:23 +0530

    Update vpc.tf

--- GCP/modules/vpc/vpc.tf
+++ GCP/modules/vpc/vpc.tf
@@ -1,27 +1,34 @@
 resource "google_compute_network" "xcloud-vpc" {
-  name = "xcloud-vpc"
-  auto_create_subnetworks = "false"
+  name                    = "xcloud-vpc"
+  auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "xcloud-subnet" {
- name          = "${var.name}-subnet"
- ip_cidr_range = "10.0.0.0/24"
- network       = google_compute_network.xcloud-vpc.id
-#  depends_on    = ["google_compute_network.xcloud-vpc"]
- region      = "${var.region}"
+  name          = "${var.name}-subnet"
+  ip_cidr_range = "10.0.0.0/24"
+  network       = google_compute_network.xcloud-vpc.id
+  region        = var.region
 }
 
-resource "google_compute_firewall" "xcloud-vpc-firewall" {
-  name    = "xcloud-firewall"
+# GCP VPN Gateway
+resource "google_compute_vpn_gateway" "xcloud_vpn" {
+  name    = "xcloud-vpn"
+  region  = var.region
   network = google_compute_network.xcloud-vpc.id
-
-  allow {
-    protocol = "icmp"
-  }
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "8080", "1000-2000"]
-  }
-  source_tags = ["web"]
 }
 
+resource "google_compute_vpn_tunnel" "gcp_aws_tunnel" {
+  name          = "gcp-aws-vpn-tunnel"
+  region        = var.region
+  vpn_gateway   = google_compute_vpn_gateway.xcloud_vpn.id
+  peer_ip       = var.aws_vpn_public_ip
+  shared_secret = var.vpn_shared_secret
+  ike_version   = 2
+}
+
+resource "google_compute_route" "aws_route" {
+  name       = "route-to-aws"
+  network    = google_compute_network.xcloud-vpc.id
+  dest_range = "10.0.0.0/16"
+  next_hop_vpn_tunnel = google_compute_vpn_tunnel.gcp_aws_tunnel.id
+}
