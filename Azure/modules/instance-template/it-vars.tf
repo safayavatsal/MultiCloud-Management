7b58df817fc7c4586816bbd7e8d557400d05a127
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-08 20:17:30 +0530

    Update it-vars.tf

--- Azure/modules/instance-template/it-vars.tf
+++ Azure/modules/instance-template/it-vars.tf
@@ -1,12 +1,57 @@
-variable "name" {}
-variable "resource_group" {}  # Azure requires a resource group
-variable "vnet_name" {}  # Azure Virtual Network
-variable "subnet_id" {}  # Azure uses Subnet ID instead of `self_link`
-variable "instance_type" {}
-variable "enable_autoscaling" {}
-variable "env" {}
-variable "region" {}
-variable "image" {}
-variable "admin_username" { default = "azureuser" }  # Default admin user for VM
-variable "admin_password" {}  # Required if using password authentication
+variable "name" {
+  description = "Name prefix for the Azure VM Scale Set"
+  type        = string
+}
 
+variable "resource_group" {
+  description = "Azure Resource Group"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Virtual Network Name"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Azure Subnet ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Azure VM Size"
+  type        = string
+}
+
+variable "enable_autoscaling" {
+  description = "Enable or disable autoscaling"
+  type        = bool
+  default     = false
+}
+
+variable "env" {
+  description = "Deployment environment (e.g., dev, staging, prod)"
+  type        = string
+}
+
+variable "region" {
+  description = "Azure Region"
+  type        = string
+}
+
+variable "image" {
+  description = "OS Image for the VM"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Default admin username for the VM"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "admin_password" {
+  description = "Admin password (if using password authentication)"
+  type        = string
+  sensitive   = true
+}
