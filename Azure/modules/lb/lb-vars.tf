d28959928434131ac475f8358083e88ff058b68b
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-08 18:59:11 +0530

    Update lb-vars.tf

--- Azure/modules/lb/lb-vars.tf
+++ Azure/modules/lb/lb-vars.tf
@@ -1,7 +1,30 @@
 variable "name" {}
-variable "resource_group" {}  # Azure requires a resource group
-variable "region" {}
-variable "webserver_count" {}
-variable "vmss_id" {}  # Azure uses VM Scale Sets instead of instance templates
-variable "zones" { type = list }
 
+variable "resource_group" {
+  description = "Azure requires a resource group"
+}
+
+variable "region" {}
+
+variable "webserver_count" {}
+
+variable "vmss_id" {
+  description = "Azure uses VM Scale Sets instead of instance templates"
+}
+
+variable "zones" { 
+  type = list
+}
+
+# Multi-cloud Load Balancing & Failover
+variable "enable_cross_cloud_lb" {
+  description = "Enable cross-cloud load balancing with AWS and GCP"
+  type        = bool
+  default     = false
+}
+
+variable "enable_failover" {
+  description = "Enable failover routing in case Azure region goes down"
+  type        = bool
+  default     = true
+}
