001fb74afaa5773f6de70380c0e8680092fff0d6
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-08 18:37:49 +0530

    Update database.tf

--- GCP/modules/database/database.tf
+++ GCP/modules/database/database.tf
@@ -6,23 +6,22 @@ resource "google_sql_database_instance" "postgres_db" {
   name             = "postgres-instance-${random_id.db_name_suffix.hex}"
   database_version = "POSTGRES_11"
   deletion_protection = false
+
   settings {
     tier = "db-f1-micro"
-
-    ip_configuration {
-      
-        authorized_networks {
-          value           = "0.0.0.0/0"
-          name            = "all"
-        }
-
+    backup_configuration {
+      enabled    = true
+      start_time = "03:00"
     }
   }
+
+  replica_configuration {
+    failover_target = true
+  }
 }
+
 resource "google_sql_user" "users" {
   name     = var.db_user
   instance = google_sql_database_instance.postgres_db.name
   password = var.db_password
-  
-
 }
