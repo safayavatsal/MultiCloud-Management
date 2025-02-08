8c43e58135edbed88358ba69d06f3125544689a1
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-08 18:37:08 +0530

    Update database.tf

--- Azure/modules/database/database.tf
+++ Azure/modules/database/database.tf
@@ -8,18 +8,17 @@ resource "azurerm_postgresql_flexible_server" "postgres_db" {
   resource_group_name    = var.resource_group
   administrator_login    = var.db_user
   administrator_password = var.db_password
-  sku_name               = "B_Standard_B1ms" # Equivalent to "db-f1-micro" in Azure
+  sku_name               = "B_Standard_B1ms"
   version                = "11"
   storage_mb             = 32768 # 32 GB storage
 
   backup_retention_days  = 7
-  geo_redundant_backup_enabled = false
+  geo_redundant_backup_enabled = true
 }
 
-resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_all" {
-  name                = "allow-all"
-  server_id          = azurerm_postgresql_flexible_server.postgres_db.id
-  start_ip_address   = "0.0.0.0"
-  end_ip_address     = "255.255.255.255"
+resource "azurerm_postgresql_flexible_server_replica" "geo_replica" {
+  name                = "azure-gcp-replica"
+  resource_group_name = var.resource_group
+  location            = "centralus"
+  source_server_id    = azurerm_postgresql_flexible_server.postgres_db.id
 }
-
