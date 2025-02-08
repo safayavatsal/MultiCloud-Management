5f24066e898df5a2bc3057fc64930335c8275f07
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-08 20:10:20 +0530

    Update instance-template.tf

--- GCP/modules/instance-template/instance-template.tf
+++ GCP/modules/instance-template/instance-template.tf
@@ -1,17 +1,13 @@
 data "template_file" "init" {
-  template = "${file("${path.module}/scripts/startup.sh")}"
+  template = file("${path.module}/scripts/startup.sh")
 }
 
 resource "google_compute_instance_template" "ws-instance_template" {
-  count = var.enable_autoscaling ? 1:0
+  count        = var.enable_autoscaling ? 1 : 0
   name         = "${var.name}-webserver-instance-template"
   project      = var.project
   machine_type = var.instance_type
   region       = var.region
-
-  metadata = {
-    # ssh-keys = "${var.user}:${file("${var.ssh_key}")}"
-  }
 
   disk {
     source_image = var.image
@@ -20,15 +16,15 @@ resource "google_compute_instance_template" "ws-instance_template" {
   }
 
   network_interface {
-    network            = var.vpc-name
-    subnetwork = var.subnet-self_link
-    
+    network    = var.vpc_name
+    subnetwork = var.subnet_self_link
+
     access_config {
-      # Ephemeral IP - leaving this block empty will generate a new external IP and assign it to the machine
+      # Assign an ephemeral public IP
     }
   }
 
-  metadata_startup_script = "${data.template_file.init.rendered}"
+  metadata_startup_script = data.template_file.init.rendered
 
   tags = ["http"]
 
