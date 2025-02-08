a999cf46ae6c11c9a81462d12bde4dfb01f04712
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-08 20:18:33 +0530

    Update it-vars.tf

--- GCP/modules/instance-template/it-vars.tf
+++ GCP/modules/instance-template/it-vars.tf
@@ -1,10 +1,45 @@
-variable name {}
-variable project {}
-variable vpc-name {}
-variable subnet-self_link{}
-variable instance_type {}
-variable enable_autoscaling {}
-variable env {}
-variable region {}
-variable image {}
+variable "name" {
+  description = "Name prefix for the GCP instance template"
+  type        = string
+}
 
+variable "project" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "VPC Name"
+  type        = string
+}
+
+variable "subnet_self_link" {
+  description = "Subnet self-link for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "GCP Instance Type"
+  type        = string
+}
+
+variable "enable_autoscaling" {
+  description = "Enable or disable autoscaling"
+  type        = bool
+  default     = false
+}
+
+variable "env" {
+  description = "Deployment environment (e.g., dev, staging, prod)"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+}
+
+variable "image" {
+  description = "OS Image for the instance"
+  type        = string
+}
