6961defabccc622d656ba240244092751c972aa6
safayavatsal <48085595+safayavatsal@users.noreply.github.com>
2025-02-08 18:43:03 +0530

    Update vpc-vars.tf

--- Azure/modules/vpc/vpc-vars.tf
+++ Azure/modules/vpc/vpc-vars.tf
@@ -2,4 +2,19 @@ variable "name" {}
 variable "region" {}
 variable "resource_group_name" {}
 
+# VPN variables for Multi-Cloud Connectivity
+variable "aws_vpn_public_ip" {
+  description = "Public IP of the AWS VPN Gateway"
+  type        = string
+}
 
+variable "gcp_vpn_public_ip" {
+  description = "Public IP of the GCP VPN Gateway"
+  type        = string
+}
+
+variable "vpn_shared_secret" {
+  description = "Shared secret for VPN tunnels"
+  type        = string
+  sensitive   = true
+}
